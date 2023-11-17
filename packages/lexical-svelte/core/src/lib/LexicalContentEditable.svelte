33404b8d86b3d64e7b70ab1f299ed2612a12b87b
June Park <junepark202012@gmail.com>
2023-11-17 20:42:15 +0900

    feat: implement LexicalContentEditable.svelte

---
+++ packages/lexical-svelte/core/src/lib/LexicalContentEditable.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	// type Props = {
+	// 	ariaActiveDescendant?: React.AriaAttributes['aria-activedescendant'];
+	// 	ariaAutoComplete?: React.AriaAttributes['aria-autocomplete'];
+	// 	ariaControls?: React.AriaAttributes['aria-controls'];
+	// 	ariaDescribedBy?: React.AriaAttributes['aria-describedby'];
+	// 	ariaExpanded?: React.AriaAttributes['aria-expanded'];
+	// 	ariaLabel?: React.AriaAttributes['aria-label'];
+	// 	ariaLabelledBy?: React.AriaAttributes['aria-labelledby'];
+	// 	ariaMultiline?: React.AriaAttributes['aria-multiline'];
+	// 	ariaOwns?: React.AriaAttributes['aria-owns'];
+	// 	ariaRequired?: React.AriaAttributes['aria-required'];
+	// 	autoCapitalize?: HTMLDivElement['autocapitalize'];
+	// 	'data-testid'?: string | null | undefined;
+	// } & React.AllHTMLAttributes<HTMLDivElement>;
+
+	import type { AriaAttributes } from 'svelte/elements';
+	import { getLexicalComposerContext } from '$lib/LexicalComposerContext';
+	import { onDestroy, onMount } from 'svelte';
+
+	export let ariaActiveDescendant: AriaAttributes['aria-activedescendant'] = undefined;
+	export let ariaAutoComplete: AriaAttributes['aria-autocomplete'] = undefined;
+	export let ariaControls: AriaAttributes['aria-controls'] = undefined;
+	export let ariaDescribedBy: AriaAttributes['aria-describedby'] = undefined;
+	export let ariaExpanded: AriaAttributes['aria-expanded'] = undefined;
+	export let ariaLabel: AriaAttributes['aria-label'] = undefined;
+	export let ariaLabelledBy: AriaAttributes['aria-labelledby'] = undefined;
+	export let ariaMultiline: AriaAttributes['aria-multiline'] = undefined;
+	export let ariaOwns: AriaAttributes['aria-owns'] = undefined;
+	export let ariaRequired: AriaAttributes['aria-required'] = undefined;
+	export let autocapitalize: HTMLDivElement['autocapitalize'] = 'off';
+	export let dataTestid: string | null | undefined = undefined;
+
+	export let role: HTMLDivElement['role'] = null;
+
+	const [editor] = getLexicalComposerContext();
+	let isEditable = false;
+	let cleanup: ReturnType<typeof editor.registerEditableListener> = () => {};
+
+	onMount(() => {
+		editor.isEditable();
+		cleanup = editor.registerEditableListener((currentIsEditable) => {
+			isEditable = currentIsEditable;
+		});
+	});
+	onDestroy(() => {
+		cleanup();
+	});
+</script>
+
+<!-- svelte-ignore a11y-aria-activedescendant-has-tabindex -->
+<div
+	{...$$restProps}
+	aria-activedescendant={!isEditable ? undefined : ariaActiveDescendant}
+	aria-autocomplete={!isEditable ? 'none' : ariaAutoComplete}
+	aria-controls={!isEditable ? undefined : ariaControls}
+	aria-describedby={ariaDescribedBy}
+	aria-expanded={!isEditable ? undefined : role === 'combobox' ? !!ariaExpanded : undefined}
+	aria-label={ariaLabel}
+	aria-labelledby={ariaLabelledBy}
+	aria-multiline={ariaMultiline}
+	aria-owns={!isEditable ? undefined : ariaOwns}
+	aria-readonly={!isEditable ? true : undefined}
+	aria-required={ariaRequired}
+	{autocapitalize}
+	contentEditable={isEditable}
+	data-testid={dataTestid}
+/>
