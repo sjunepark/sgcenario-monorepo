f5085764dbaa2374551fcfc49b12f640eede1f26
June Park <junepark202012@gmail.com>
2023-11-18 14:17:09 +0900

    save: before svelte5

---
+++ packages/lexical-svelte/core/src/lib/LexicalTreeView.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { LexicalCommand, LexicalEditor } from 'lexical';
+	import { createLoggedCommandsStore, useLexicalCommandsLog } from '$lib/LexicalTreeViewStore';
+	import { COMMAND_PRIORITY_HIGH } from 'lexical';
+
+	export let editor: LexicalEditor;
+	const commandsLog = useLexicalCommandsLog(editor);
+</script>
